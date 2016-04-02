c5a9a9d8d3acb62e15a08d64b617764d5aaff298
Angus Lam <angushtlam@gmail.com>
2016-04-02 10:17:56 -0400

    Working calendar site

--- project/project.py
+++ project/project.py
@@ -1,4 +1,5 @@
 # https://github.com/mimming/python-flask-google-api-starter/blob/master/cal.py
+# https://developers.google.com/api-client-library/python/auth/web-app#example
 
 # Required for Heroku
 import os
@@ -6,7 +7,10 @@ import os
 import apiclient
 import httplib2
 # OAuth imports
-from oauth2client.client import OAuth2WebServerFlow
+from oauth2client import client
+# Other imports
+import json
+import datetime
 # Flask imports
 from flask import Flask, render_template, session, request, redirect, url_for
 
@@ -16,65 +20,72 @@ CLIENT_ID = "788402987571-lkij3nh54tlp35g82h3b94ktj6gl529g.apps.googleuserconten
 CLIENT_SECRET = '0EXqx_OQbJYRw-TavXBftK60'
 
 
-@app.route('/login')
-def login():
-    flow = OAuth2WebServerFlow(client_id=CLIENT_ID,
-                               client_secret=CLIENT_SECRET,
-                               scope='https://www.googleapis.com/auth/calendar',
-                               redirect_uri='https://lovely-hackprinceton.herokuapp.com:'
-                                            + os.environ.get('PORT', 5000) + '/oauth2callback',
-                               approval_prompt='force',
-                               access_type='offline')
+@app.route('/')
+def index():
+    # Check if credentials exist within session.
+    if 'credentials' not in session:
+        return redirect(url_for('oauth2callback'))
 
-    auth_uri = flow.step1_get_authorize_url()
-    return redirect(auth_uri)
+    # Convert json credentials stored to a OAuth2Credentials object
+    credentials = client.OAuth2Credentials.from_json(session['credentials'])
 
+    # If the credentials are expired, they're might as well nonexistent!
+    if credentials.access_token_expired:
+        return redirect(url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        service = apiclient.discovery.build('calendar', 'v3', http_auth)
 
-@app.route('/signout')
-def signout():
-    del session['credentials']
-    session['message'] = "You have logged out"
+        '''
+        Shows basic usage of the Google Calendar API.
 
-    return redirect(url_for('index'))
+            Creates a Google Calendar API service object and outputs a list of the next
+            10 events on the user's calendar.
+        '''
+
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+
+        events_result = service.events().list(
+            calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        return render_template('index.html', events=events)
 
 
 @app.route('/oauth2callback')
 def oauth2callback():
-    code = request.args.get('code')
+    # Exchange the authorization code for user credentials
+    flow = client.OAuth2WebServerFlow(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        scope='https://www.googleapis.com/auth/calendar',
+        redirect_uri=url_for('oauth2callback', _external=True),
+        access_type='offline'
+    )
 
-    if code:
-        # exchange the authorization code for user credentials
-        flow = OAuth2WebServerFlow(CLIENT_ID,
-                                   CLIENT_SECRET,
-                                   "https://www.googleapis.com/auth/calendar")
-        flow.redirect_uri = request.base_url
+    # If a credential code is not received from Google yet, send them to get it.
+    if 'code' not in request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return redirect(auth_uri)
 
-        try:
-            credentials = flow.step2_exchange(code)
-        except Exception as e:
-            print("Unable to get an access token because ", e.message)
+    # Otherwise, authenticate the user.
+    else:
+        auth_code = request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
 
-        # store these credentials for the current user in the session
-        # This stores them in a cookie, which is insecure. Update this
-        # with something better if you deploy to production land
-        session['credentials'] = credentials
+        # Convert credentials to json because that is the only way to store it in a session.
+        session['credentials'] = credentials.to_json()
+
+        return redirect(url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    del session['credentials']
+    session['message'] = "You have logged out."
 
     return redirect(url_for('index'))
-
-
-@app.route('/')
-def index():
-    credentials = session['credentials']
-
-    if credentials is None:
-        return redirect(url_for('login'))
-
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-    service = apiclient.discovery.build("calendar", "v3", http=http)
-    calendar_list = service.calendarList().list().execute()
-
-    return render_template("index.html", calendar_list=calendar_list)
 
 
 if __name__ == '__main__':
@@ -82,5 +93,3 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.secret_key = "Love.ly"
     app.run(host='0.0.0.0', port=port, debug=True)
-
-
