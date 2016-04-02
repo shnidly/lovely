0ce5dba94bca71c98d6f486a26049d964d529400
Angus Lam <angushtlam@gmail.com>
2016-04-02 01:25:47 -0400

    Fixed debug

--- project/project.py
+++ project/project.py
@@ -81,7 +81,6 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.secret_key = "Love.ly"
-    app.debug(True)
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
