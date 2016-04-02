d22af72d3a8093a215b1a22360fe4ecdbe360bcd
Angus Lam <angushtlam@gmail.com>
2016-04-02 00:19:48 -0400

    Heroku port fix

--- project/project.py
+++ project/project.py
@@ -10,5 +10,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
