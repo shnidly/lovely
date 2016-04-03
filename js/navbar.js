80428060cd5718bcbeffae72ce2d408903193084
mdfanelle <mdfanelle@gmail.com>
2016-04-03 00:10:19 -0400

    before i lose things again....

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,28 @@
 function navbar () {
 	alert("here");
-
-	var input = "<ul class="sidebar-menu" id="nav">                
-                  <li class="active">
+/*
+  var content = '<li class="active">
                       <a class="" href="index.html">
-                          <span>Person 1</span>
+                          <span>Friend 1</span>
                           <span class="glyphicon glyphicon-heart" style="color: #462066"></span>
                       </a>
                   </li>
                   <li class="sub-menu">
                       <a href="javascript:;" class="">
-                          <span>Person 2</span>
+                          <span>Friend 2</span>
                       </a>
                   </li>
 
                   <li class="sub-menu">
                       <a href="javascript:;" class="">
-                          <span>Person 3</span>
+                          <span>Friend 3</span>
                       </a>
                   </li>         
                   </ul>   
                   <div id="plus">
 
                   <a href="#"><div class="round-button" alt="add contact"><span class="glyphicon glyphicon-plus"></span></div></a>
-              </div>";
+              </div>';
 
- document.getElementById("nav").innerHTML = input;
+  document.getElementById("nav").innerHTML = content;*/
  }
